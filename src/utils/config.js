3a38f0946c37f47f25d08ba4e4744652826ee468
Mohammad Dayarneh <m.daya.nutrino@gmail.com>
2025-04-21 01:22:44 +0400

    Test

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,2 +1,2 @@
-export const BASE_URL = process.env.REACT_APP_BASE_URL;
-export const JWT_SECRET = process.env.JWT_SECRET;
+export const BASE_URL = "https://jordan-tourism-backend.onrender.com/api/v1"
+export const JWT_SECRET = "14c03ab4060863f29c3f8aef90697141abe269b502aa8e591ff806d99848b0ee693f9f0100d7b9e9e586fd540d9b2ccaef288a3f639254f2a62c3537e144b8cd"
