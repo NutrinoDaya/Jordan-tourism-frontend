49c85b4898c332ab338161a3d73eb997f20376e4
Mohammad Dayarneh <m.daya.nutrino@gmail.com>
2025-06-18 00:41:42 +0400

    Update

--- src/utils/config.js
+++ src/utils/config.js
@@ -1,3 +1,3 @@
 export const BASE_URL = "https://jordan-tourism-backend.onrender.com/api/v1"
 export const JWT_SECRET = "14c03ab4060863f29c3f8aef90697141abe269b502aa8e591ff806d99848b0ee693f9f0100d7b9e9e586fd540d9b2ccaef288a3f639254f2a62c3537e144b8cd"
-export const NGROK = "https://2e7d-94-207-225-249.ngrok-free.app"
+export const NGROK = "https://e8ae-94-207-225-249.ngrok-free.app"
